80ced6ea93efcf935c462f641a004d0f50b87143
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-11-03 19:27:00 -0300

    Testes do model

--- test/models/veiculo_test.rb
+++ test/models/veiculo_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class VeiculoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'cadastro de um veiculo' do
+    @veiculo = Veiculo.new(marca: 'Cadillac', veiculo: 'automovel', ano: 1991,
+                           descricao: 'Deville/Eldorado 4.9', vendido: false)
+    assert_difference 'Veiculo.count' do
+      @veiculo.save
+    end
+  end
+
+  test 'não cadastra um veiculo invalido' do
+    @veiculo = Veiculo.new(marca: 'Cadillac', veiculo: 'automovel', vendido: false)
+
+    assert_difference 'Veiculo.count', 0 do
+      @veiculo.save
+    end
+  end
 end
