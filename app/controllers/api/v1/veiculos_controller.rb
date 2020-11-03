ad2cdb924417cfa9e0d26fee84342046095e21d7
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-11-03 18:59:56 -0300

    Exbição das informações de apenas um veiculo

--- app/controllers/api/v1/veiculos_controller.rb
+++ app/controllers/api/v1/veiculos_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::VeiculosController < ApplicationController
+  before_action :set_veiculo, only: %i[show update destroy]
+
   def index
     @veiculos = Veiculo.all
     render json: @veiculos
+  end
+
+  def show
+    render json: @veiculo
   end
 
   def create
@@ -15,14 +21,14 @@ class Api::V1::VeiculosController < ApplicationController
   end
 
   def update
-    @veiculo = Veiculo.find(params[:id])
-    @veiculo.update(veiculos_params)
-
-    render json: { 'success': true, 'status': 'atualizado' }
+    if @veiculo.update(veiculos_params)
+      render json: @veiculo
+    else
+      render json: @veiculo.errors
+    end
   end
 
   def destroy
-    @veiculo = Veiculo.find(params[:id])
     @veiculo.destroy
 
     render json: { 'success': true, 'status': 'deletado' }
@@ -32,5 +38,9 @@ class Api::V1::VeiculosController < ApplicationController
 
   def veiculos_params
     params.require(:veiculo).permit(:marca, :veiculo, :ano, :descricao, :vendido)
+  end
+
+  def set_veiculo
+    @veiculo = Veiculo.find(params[:id])
   end
 end
