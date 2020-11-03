ba400ffa5c1c8b1329871416f955f05c674355d6
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-11-03 19:55:51 -0300

    Testes do controller

--- test/controllers/api/v1/veiculos_controller_test.rb
+++ test/controllers/api/v1/veiculos_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class Api::V1::VeiculosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'cadastrar um veiculo' do
+    veiculo = { marca: 'Cadillac', veiculo: 'automovel', ano: 1991,
+                descricao: 'Deville/Eldorado 4.9', vendido: false }
+
+    assert_difference 'Veiculo.count' do
+      post api_v1_veiculos_url, params: { veiculo: veiculo }
+    end
+  end
+
+  test 'listar veiculos' do
+    get api_v1_veiculos_url
+    assert_response :success
+  end
+
+  test 'detalhes de um veiculo' do
+    get api_v1_veiculo_url(id: veiculos(:one).id)
+    assert_response :success
+  end
+
+  test 'alterar um veiculo' do
+    put api_v1_veiculo_url(id: veiculos(:one).id), params: { veiculo: { vendido: true } }
+    assert_response :success
+  end
+
+  test 'remove um veiculo' do
+    assert_difference 'Veiculo.count', -1 do
+      delete api_v1_veiculo_url(id: veiculos(:one).id)
+    end
+  end
 end
