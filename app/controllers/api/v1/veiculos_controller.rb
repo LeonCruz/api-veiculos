30595690c403301b9eea9f7b51c6d52955526f70
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-11-03 18:21:25 -0300

    Cadastro de veículos

--- app/controllers/api/v1/veiculos_controller.rb
+++ app/controllers/api/v1/veiculos_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::VeiculosController < ApplicationController
+  def create
+    @veiculo = Veiculo.new veiculos_params
+
+    if @veiculo.save
+      render json: @veiculo
+    else
+      render json: @veiculo.errors
+    end
+  end
+
+  private
+
+  def veiculos_params
+    params.require(:veiculo).permit(:marca, :veiculo, :ano, :descricao, :vendido)
+  end
 end
--- app/models/veiculo.rb
+++ app/models/veiculo.rb
@@ -1,2 +1,4 @@
 class Veiculo < ApplicationRecord
+  validates_presence_of :marca, :veiculo, :ano, :descricao
+  validates :vendido, inclusion: [true, false]
 end
